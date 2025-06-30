614c72cbeaee07d41ce851baad65fd5c6c0d287a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-30 18:41:21 +0100

    refactor(Search): simplify search component markup
    Remove input field and styling, leaving only a styled div placeholder

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Search = () => {
     return (
-        <div>
-            <input type="text" placeholder="Search for a movie" className="w-full px-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-        </div>
+        <div className="text-white text-3xl"></div>
     );
 };
 
