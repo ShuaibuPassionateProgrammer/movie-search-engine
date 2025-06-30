58b18574f74b076a5f1382cc184074989dc68eb6
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-06-30 19:07:23 +0100

    feat(Search): add person object for testing purposes

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+const person = {
+    name: "Bruce Lee",
+    age: 36,
+    location: "Gotham city"
+};
+
 const Search = (props) => {
     return (
         <div className="text-white text-3xl">{props.searchTerm}</div>
