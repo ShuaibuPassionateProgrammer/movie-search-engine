f961c657528091e536bb5dfc32212ce28a5a8739
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-01 18:25:06 +0100

    The wrapper class is changed to search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -12,7 +12,7 @@ console.log(name);
 
 const Search = (props) => {
     return (
-        <div className="text-white text-3xl">{props.searchTerm}</div>
+        <div className="search"></div>
     );
 };
 
