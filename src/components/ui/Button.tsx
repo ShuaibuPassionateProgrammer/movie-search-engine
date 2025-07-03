dd0d542938a2c1d9fbe41750b0a4263736519479
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-03 18:04:47 +0100

    The function that trigers the Button action is created

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 const Button = () => {
     const [name, setName] = useState("Shuaibu");
 
+    const handleChangeName = () => {};
+
     return (
         <div>
             <h1>{name}</h1>
