df8dd728c2735be35005681aa9fb73478935e07f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-03 05:52:49 +0100

    style: remove extra whitespace in App component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const App = () => {
     return (
         <div className="container">
             <button onClick={() => setHasLiked({ userLike: true })}>Like</button>
+            
         </div>
     );
 };
