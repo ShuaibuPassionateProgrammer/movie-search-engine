b96f653c11a8e42248ad6105ac73521e5cf17be6
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-03 19:05:52 +0100

    refactor: remove Card component implementation

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,18 +1,3 @@
 
 import React from "react";
 
-export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
-  children: React.ReactNode;
-}
-
-export const Card = React.forwardRef<HTMLDivElement, CardProps>(
-  ({ children, ...props }, ref) => {
-    return (
-      <div ref={ref} {...props}>
-        {children}
-      </div>
-    );
-  }
-);
-
-Card.displayName = "Card";
