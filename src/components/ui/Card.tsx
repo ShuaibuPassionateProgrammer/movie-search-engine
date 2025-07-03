ba8ad994e589f5e82e85b40e2a7ab327a8a3e501
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-03 19:07:29 +0100

    refactor: clean up Card component return statement

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -7,6 +7,10 @@ export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export const Card = React.forwardRef<HTMLDivElement, CardProps>(
   ({ children, ...props }, ref) => {
-    
+    return (
+      <div ref={ref} {...props}>
+        {children}
+      </div>
+    );
   }
 );
