b4ff3888451bf3f423b2a39332eb21b37b7987e4
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-03 18:32:07 +0100

    feat: add loading icon to Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -66,7 +66,13 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
               stroke="currentColor"
               strokeWidth="4"
             />
-            
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+            />
+          </svg>
+        )}
       </button>
     );
   }
