3e00aca44cef8a8a7d4f134332db877ef38a9f66
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-03 19:07:10 +0100

    feat: implement Card component with children prop

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -5,3 +5,8 @@ export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
 }
 
+export const Card = React.forwardRef<HTMLDivElement, CardProps>(
+  ({ children, ...props }, ref) => {
+    
+  }
+);
