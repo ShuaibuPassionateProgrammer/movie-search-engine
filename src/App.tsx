a789cf281501269e6046cd53fa463ffce9b1926f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-03 05:41:21 +0100

    refactor: remove unused App component content

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,3 @@
-import React, { useState } from "react";
-import Search from "./components/Search";
-
-const App = () => {
-    const [searchTerm, setSearchTerm] = useState("");
-
-    return (
-        <main className="pattern">
-            <div className="wrapper">
-                <header>
-                    <h1>Find the <span className="text-gradient">movies</span> you'll enjoy without the Hassle</h1>
-                    <img src="/vite.svg" alt="The Vite Logo"/>
-                </header>
-
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-                <h1 className="text-white">{searchTerm}</h1>
-            </div>
-        </main>
-    );
-};
+const App = () => {};
 
 export default App;
